45633c0fb93ea38b9f1a40b45e653c101489fba7
Samuel Jackson <samueljackson@outlook.com>
2013-02-04 16:11:56 +0000

    Updating to use foauth

--- fb_graph.py
+++ fb_graph.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python2.7
-import facebook
 import fb_auth
-from bcolors import bcolors
 from simple_graph import *
+from bcolors import bcolors
 
 if __name__ == "__main__":
-	ACCESS_TOKEN = fb_auth.authorize()
-    
-	graph = facebook.GraphAPI(ACCESS_TOKEN)
-	profile = graph.get_object("me")
-	friends = graph.get_connections("me", "friends")
+
+	#Collect Username and Password
+	fb_auth.auth()
+
+	#Get Profile and Friends list
+	profile = fb_auth.queryJSON('me')
+	friends = fb_auth.queryJSON('me/friends')
 	friends = friends['data']
 
+	#Create graph of the data
 	fg = Graph()
 	fg.add_node(profile['id'], Node(profile))
 
@@ -20,24 +22,37 @@ if __name__ == "__main__":
 		fg.add_node(f["id"], Node(f))
 		fg.add_adjacent(profile['id'], fg.get_node(f["id"]))
 
+	#variables for friend stats
 	average_mutual = 0
 	max_mcount = 0
 	max_mutual = None
+
+	#build graph for mutual friends
 	for f in friends:
+		#if not us
 		if f['id'] != profile['id']:
+
+			#get list of mutual friends
 			print "Examining: ", f['name']
-			mutualfriends = graph.get_connections(f['id'], "mutualfriends")
+			mutualfriends = fb_auth.queryJSON(f['id']+"/mutualfriends")
+
+			#check if they have the most mutual friends
 			mcount = len(mutualfriends['data'])
 			if mcount > max_mcount:
 				max_mutual = f['name']
 				max_mcount = mcount
 
 			average_mutual += mcount
-			print "--------->  %s%d mutual friends%s" % (bcolors.PURPLE, mcount, bcolors.ENDC)		
+
+			#a little output 
+			print "--------->  %s%d mutual friends%s" % (bcolors.PURPLE, mcount, bcolors.ENDC)
+
+			#add connections in graph between friends	
 			for m in mutualfriends['data']:
 				if f['id'] != m['id']:
 					fg.add_adjacent(f['id'], fg.get_node(m['id']))
 
+	#output friend stats
 	average_mutual /= len(friends)-1
 	print "\n\nAverage Number of Mutual Friends: 	%s" % average_mutual
 	print "Max Number of Mutual Friends:		%s" % max_mutual
@@ -48,14 +63,14 @@ if __name__ == "__main__":
 
 	stroutput = '<gexf xmlns="http://www.gexf.net/1.2draft" version="1.2" xmlns:viz="http://www.gexf.net/1.2draft/viz"><graph mode="static" defaultedgetype="undirected"><nodes>\n'
 
-	#nodes
+	#output nodes
 	for i, n in fg.nodes.iteritems():
 		stroutput += '<node id="' + n.data["id"] + '" label="'+ n.data["name"]+'" />\n'
 
 	stroutput += '</nodes>\n'
 	stroutput += '<edges>\n'
 
-	#edges
+	#output edges
 	count = 0
 	for i, n in fg.nodes.iteritems():
 		if n.data['id'] == profile['id']:
@@ -69,6 +84,8 @@ if __name__ == "__main__":
 			stroutput +='</edge>'
 
 	stroutput += '</edges></graph></gexf>\n'
+
+	stroutput = stroutput.decode("utf-8")
 
 	f = open("output.gexf", 'w')
 	f.write(stroutput)
